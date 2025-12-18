911e74eb07eeeb9ab6ac9478a7ead8a330973434
victorolorunshola8-cyber <victorolorunshola8@gmail.com>
2025-12-18 03:05:33 +0100

    Complete frontend login and registration pages

--- Secure-Login-System/app.py
+++ Secure-Login-System/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return "Secure Login System Running!"
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
